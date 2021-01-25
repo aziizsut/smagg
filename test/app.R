f101423ea79408299c1fe685004c68d8168e5e3e
Aziiz Sut <aziiz.ti06@gmail.com>
2021-01-25 11:37:18 +0700

    update price

--- test/app.R
+++ test/app.R
@@ -1,251 +1,184 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinydashboard)
+library(dygraphs)
+library(datasets)
 
-ui<-fluidPage(
-    titlePanel("Waiting List Volume"),
-    hr(style="border-color: grey;"),
-    sidebarLayout(
-        # panel with all inputs
-        sidebarPanel(selectInput("fixed","System type",choices=c("Fixed","Random")),
-                     conditionalPanel(condition = "input.fixed == 'Random'",
-                                      numericInput("seed","Set seed",0)),
-                     numericInput("mean","Weekly arrivals",1000, step = 100),
-                     conditionalPanel(condition = "input.fixed == 'Random'",
-                                      numericInput("sda","Standard deviation",50, step = 10)),
-                     numericInput("apps","Weekly appointments",900,step = 100),
-                     conditionalPanel(condition = "input.fixed == 'Random'",
-                                      numericInput("sdt","Standard deviation",50, step = 10)),
-                     numericInput("dna","DNA rate %",20,min=0,max=100,step=5),
-                     helpText("Patients are treated in turn, unless they DNA"),
-                     hr(style="border-color: grey;"),
-                     # start / reset buttons on same row
-                     fluidRow(
-                         column(7,uiOutput("resetbutton")),
-                         column(5,uiOutput("startbutton"))
-                     ),
-                     hr(style="border-color: grey;"),
-                     fluidRow(
-                         column(7,numericInput("skipnum",NA,10)),
-                         column(5,actionButton("skip","Skip"))
-                     ),
-                     fluidRow(
-                         column(7,actionButton("stop","Stop")),
-                         column(3,actionButton("play","Play"))
-                     )
-        ),
-        # plot panel
-        mainPanel(
+# sidebar -----------------------------------------------------------------
+
+
+sidebar <- dashboardSidebar(sidebarMenu(
+  menuItem(
+    "Introduction",
+    tabName = "intro",
+    icon = icon("power-off")
+  ), # 1st Menu item 
+  menuItem(
+    "Pricing Scenarios",
+    tabName = "pricing",
+    icon = icon("donate")
+  ), # 2nd menu item
+  menuItem(
+    "Company Flex Profile",
+    tabName = "profile",
+    icon = icon("building")
+  )
+))
+
+
+# body --------------------------------------------------------------------
+
+intro1 <- "This is a simple mock-up for the simulation model to calculate the impact of Flex market for a company"
+
+body <- dashboardBody(
+  # Here start the dashboard application
+  tabItems(
+    
+    tabItem(tabName = "intro",
+            h2("Flexible Electricity Market", 
+               style = "font-weight: 250; color: #4d3a7d;"),
+            p(intro1,
+              style = "font-weight: 120;")),
+
+# Pricing Tabs ------------------------------------------------------------
+# In this tabs we can see the building blocks for the input mechanism
+# The substation also contains physical capacity limit of the susbstation
+    
+    tabItem(tabName = "pricing",
+            br(),
+            fluidRow(
+              column(width = 4,
+              box(title = "Pricing Scenarios", 
+                   height = 300,
+                   br(),
+                   "You may select your pricing options here",
+                   br(),
+                   radioButtons("priceScen", "Pricing Options:",
+                                c("Time of Use" = "timing",
+                                  "Critical Peak" = "unif",
+                                  "Real-Time Pricing" = "rtp",
+                                  "Flat Pricing" = "flat")),
+                  actionButton("sims", "Simulate Now", icon = icon("play-circle"))
+                                     
+            ), # box 1 closing bracket
+            box(title = "Substation Capacity", status = "warning", 
+                height = 300, solidHeader = TRUE, # line for the substation hard constraints 
+                br(),
+                sliderInput("substation", "Substation Capacity (in kVA):",
+                            min = 50, max = 100, value = 60, step = 5),
+                sliderInput("kwhPrice", "Normal Capacity Price (in Eurocent per kVA):",
+                            min = 1, max = 80, value = 12, step = 1)
+            ), # box 2 closing bracket
             
-            # tab layout
-            tabsetPanel(
-                
-                tabPanel("Distribution",plotOutput('mygraph')),
-                tabPanel("Summary",
-                         
-                         # plots on same row
-                         fluidRow(
-                             
-                             column(6,
-                                    
-                                    plotOutput('voltrack')
-                             ),
-                             column(6,
-                                    plotOutput('pie')  
-                             )
-                             
-                         )
-                         
-                )
+            box(title = "Capacity Pricing",  
+                height = 300, # line for the substation hard constraints 
+                br(),
+                numericInput("safe", "Normal Zone Limit (in kVA):", 200, min = 100, max  = 300, step = 10),
+                numericInput("warning", "Warning Zone Limit (in kVA):", 250, min = 150, max = 350, step = 10),
+                numericInput("danger", "Dangerous Zone Limit (in kVA):", 350, min = 250, max = 450, step = 10)
             ),
             
-            # visual data on same row
-            fluidRow(
-                column(3,textOutput("weekcount")),
-                column(4,textOutput("volume")),
-                column(4,textOutput("over"))
-            )
+            box(title = "Capacity Pricing Range",
+                height = 300,
+                plotOutput("capPrice"))
             
+            ), # end of column1 
+            
+            fluidRow(
+            column(width = 7,
+            box(title = "Monthly Chart", height = 500, width = 12,
+                dygraphOutput("balance")),
+            infoBox(title = "Energy Expenditure", icon = icon("donate"), value = paste0(200, "\u20AC"), color = "orange"),
+            infoBox(title = "Energy Exported to Grid", icon = icon("plug"), value = paste0(300, "kWh"), color = "orange"),
+            infoBox(title = "Flex Production used", icon = icon("charging-station"), value = paste0(200, "kWh"), color = "orange")
+            ))
+            )  # box 3 closing bracket and fluid row
+
+            
+            ),  # tabitem closing bracket
+
+  tabItem(tabName = "profile",
+        box(width = 3, status = "warning", solidHeader = TRUE,
+            title = "Company VRE Production",
+            checkboxInput("flexp", "Participating in Flex", value = FALSE),
+            numericInput("panels", "Solar Panels Installed (in kWp):", 20, min = 0, max = 999, step = 1),
+            numericInput("solaryeild", "Solar Panel Production yield (in kWh per kWp):", 850, min = 0, max = 2000, step = 50)
+        ),
+        
+        fluidRow(
+          column(width = 8,
+                
+                 box(width = 10, height = 500,
+                     status = "primary", solidHeader = TRUE,
+                     title = "Market Price Condition",
+                     dygraphOutput("pricing"))
+                 )
         )
-    )
-    
+        
+        
+        )
+
+  ) # tabitems closing
+
+) # dashboarbody closing
+
+
+# UI panel ---------------------------------------------------------
+
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Pricing Options Effect"),
+  sidebar = sidebar,
+  body = body
 )
 
-server<-function(input,output){
-    
-    waits <- reactiveValues() # reactive to store all reactive variables
-    waits$resetindicator<-0   # used to change button labels
-    waits$data <- rep(0,52)   # initial waitlist profile
-    waits$count<-0            # week number
-    waits$vol<-0              # current week volume
-    waits$trackvol<-rep(0,20) # volume for last 20 weeks
-    waits$xdates<-rep(NA,20)  # x axis for volume graph
-    waits$past52<-0           # number of expired
-    waits$slices<-c(0,0,0)    # pie chart value
-    
-    
-    
-    # dynamic reset button label
-    output$resetbutton<-renderUI({
-        if(waits$resetindicator==0){
-            lbl<-"Set Parameters"
-        }else{
-            lbl<-"Reset"
-        }
-        actionButton("reset",label=lbl)
+
+# Execution block ---------------------------------------------------------
+
+
+server <- function(input, output) {
+
+
+# capacity page section ---------------------------------------------------
+
+  dsetBalance <- reactive({
+    cbind(mdeaths, fdeaths)
     })
-    
-    # dynamic start button label
-    output$startbutton<-renderUI({
-        if(sum(waits$data)==0){
-            lbl<-"Start"
-        }else{
-            lbl<-"Next Week"
-        }
-        actionButton("nextweek",label=lbl)
-    })
-    
-    forward<-function(){
-        
-        req(input$seed)
-        req(input$mean)
-        req(input$sda)
-        req(input$apps)
-        req(input$sdt)
-        req(input$dna)
-        
-        waits$resetindicator<-1 # change button label
-        
-        
-        # set number of appointments
-        if(input$fixed=="Random"){
-            canremove<-max(round(rnorm(1,mean=input$apps,sd=input$sdt)),0)
-        }else{
-            canremove<-input$apps
-        }
-        
-        coltarget<-length(waits$data) # column to remove patients from
-        while(canremove>0){
-            colremovesize<-ceiling((1-(0.01*input$dna))*waits$data[coltarget]) # how many can be removed from column
-            removals<-min(canremove,colremovesize)
-            waits$data[coltarget]<-waits$data[coltarget]-removals
-            canremove<-canremove-removals # update number of appointments remaining
-            coltarget<-coltarget-1 # target next column
-        }
-        
-        waits$past52<-waits$past52+waits$data[52] # update expired vector
-        
-        # set number of new arrivals
-        if(input$fixed=="Random"){
-            newwait<-max(round(rnorm(1,mean=input$mean,sd=input$sda)),0)
-        }else{
-            newwait<-input$mean
-        }
-        
-        waits$data <- head(c(newwait,waits$data),52) # append new arrivals to start
-        waits$count<-waits$count+1 # update week number
-        
-        waits$slices<-c(sum(waits$data[1:25]),sum(waits$data[26:35]),sum(waits$data[36:52])) # values for pie chart
-        waits$vol<-sum(waits$data) # update volume
-        waits$trackvol<-tail(c(waits$trackvol,waits$vol),20) # vector of volumes for last 20 weeks
-        waits$xdates<-tail(c(waits$xdates,waits$count),20)
-    }
-    
-    
-    ## when nextweek button is pressed
-    observeEvent(input$nextweek,{
-        forward()
-    })
-    
-    observeEvent(input$skip,{
-        for (fwd in 1:input$skipnum){
-            forward()
-        }
-    })
-    
-    session<-reactiveValues()
-    session$timer<-reactiveTimer(Inf)
-    
-    observeEvent(input$play,{
-        session$timer<-reactiveTimer(300)
-        observeEvent(session$timer(),{
-            forward()
-        })
-    })
-    
-    
-    observeEvent(input$stop,{
-        session$timer<-reactiveTimer(Inf)
-    })
-    
-    
-    
-    
-    
-    
-    ## when reset button is pressed (set everything to original values, plus set seed)
-    observeEvent(input$reset,{
-        
-        waits$resetindicator<-0
-        
-        set.seed(input$seed) # seed set here for now as it has to be a reactive environment
-        waits$past52<-0
-        waits$data<-rep(0,52)
-        waits$count<-0
-        waits$vol<-0
-        waits$trackvol<-rep(0,20)
-        waits$xdates<-rep(NA,20)
-        waits$slices<-c(0,0,0)
-    })
-    
-    ## main plot output
-    output$mygraph<-renderPlot({
-        if(sum(waits$data)==0){
-            return() # no plot if reset
-        }
-        barcolours<-c(rep("green3",26),rep("yellow2",10),rep("red2",16))
-        bp<-barplot(waits$data,names.arg=1:52,xlab="Weeks Wait",ylab="Volume",col=barcolours,space=0,width=1)
-        bp
-        abline(v=26)
-        abline(v=36)
-    })
-    
-    ## visual data outputs
-    output$weekcount<-renderText({
-        paste("Week number",waits$count)
-    })
-    output$over<-renderText({
-        paste("Expired",waits$past52)
-    })
-    output$volume<-renderText({
-        paste("Waitlist volume",waits$vol)
-    })
-    
-    ## pie plot output
-    output$pie<-renderPlot({
-        if(sum(waits$slices)==0){
-            return() # dont error if no data
-        }
-        par(xpd=TRUE) # allow legend outside plot margins
-        pie(waits$slices,labels = waits$slices,col=c("green3","yellow2","red2"))
-        legend(-1.3,-0.5,legend=c("<26","26 to 25","36 +"),fill=c("green3","yellow2","red2"),bty='n',cex=0.8)
-    })
-    
-    output$voltrack<-renderPlot({
-        ytop<-max(waits$trackvol)+100 # upper limit for y axis
-        plot(waits$trackvol,type='l',ylab="Waitlist Volume",xaxt='n',xlab="Week Number",bty='n',lwd=1.5,ylim=c(0,ytop),main="20 Week Volume")
-        points(20,tail(waits$trackvol,1),pch=21,bg="blue")
-        axis(1,at=1:20,labels = waits$xdates)
-    })
-    
+  
+  output$balance <- renderDygraph({
+    dygraph(dsetBalance()) %>% 
+    dySeries("mdeaths", label = "Consumption") %>%
+      dySeries("fdeaths", label = "Production") %>%
+      dyOptions(stackedGraph = TRUE) %>%
+      dyRangeSelector(height = 20)
+  })
+  
+  predicted <- reactive({
+  hw <- HoltWinters(ldeaths)
+  predict(hw, n.ahead = 36, 
+          prediction.interval = TRUE,
+          level = as.numeric(0.95))
+  })
+  
+  output$pricing <- renderDygraph({
+    dygraph(predicted(), main = "Predicted Pricing/Days") %>%
+      dySeries(c("lwr", "fit", "upr"), label = "Cent per kWh") 
+  })
+  
+
+# data capacity -----------------------------------------------------------
+
+  # plotBase <- reactive({
+  #   
+  #   input$safe
+  #   
+  # })
+  # 
+  # output$capPrice <- renderPlot({
+  #   
+  #   
+  #   
+  # })
+  
 }
 
 shinyApp(ui, server)
-
